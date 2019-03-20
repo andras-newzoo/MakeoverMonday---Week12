a3db2fd9c9fee234062e1eaa2ee8702df7b25936
Andras Szesztai <andras@newzoo.com>
2019-03-20 21:51:44 +0100

    Updated

--- src/App.js
+++ src/App.js
@@ -4,17 +4,42 @@ import './App.css';
 import Chart from './components/Chart'
 import data from './data/data.json'
 
+import { select, event as currentEvent } from 'd3-selection'
+
 class App extends Component {
   constructor(props){
         super(props)
         this.state = {
+          countryGuess: [
+            {Canada: 50},
+            {France: 50}
+          ]
 
         }
     }
 
+
+  handleDrag = (d, i, n) => {
+      select(n[i]).attr("y", currentEvent.y > -7.5  && currentEvent.y < 560 ?  d.y = currentEvent.y : d.y = d.y);
+      const country = d.country.replace(' ', ''),
+            copy = {...this.state}
+            // copy.countryGuess[country] = d.y
+            // this.setState(copy)
+
+            console.log(copy.countryGuess.Canada)
+  }
+
   render() {
 
-    const countryData = data.filter(d => d.country !== 'G7 Average')
+    const countryData = data.filter(d => d.country !== 'G7 Average'),
+          { countryGuess } = this.state,
+          datatest = []
+
+    console.log(countryGuess)
+
+
+    countryData.sort((a, b) => a.country.localeCompare(b.country));
+
 
 
     return (
@@ -23,6 +48,8 @@ class App extends Component {
             data = {countryData}
             width = {1000}
             height = {600}
+            handleDrag = {this.handleDrag}
+            countryGuess = {countryGuess}
           />
       </div>
     );
--- src/components/Chart.js
+++ src/components/Chart.js
@@ -3,48 +3,85 @@ import './Chart.css';
 
 import { scaleBand, scaleLinear } from 'd3-scale'
 import { axisBottom, axisLeft } from 'd3-axis'
-import { select } from 'd3-selection'
+import { select, event as currentEvent } from 'd3-selection'
+import { transition } from 'd3-transition'
 import { format } from 'd3-format'
+import { drag } from 'd3-drag'
 
 import { updateSvg, appendArea, createUpdateYAxis, createUpdateXAxis } from './chartFunctions'
 
 class Chart extends Component {
 
+  handleDrag = (d, i, n) => {
+    this.props.handleDrag(d, i, n)
+  }
 
   componentDidMount(){
     const {data} = this.props
 
-    console.log(data)
+    //console.log(data)
     this.initVis()
   }
 
 
   componentDidUpdate(){
+    const {countryGuess} = this.props
 
+    console.log(countryGuess)
   }
+
+
+
 
   initVis(){
 
     const svg = select(this.node),
-          { data, width, height, margin } = this.props,
+          { data, countryGuess, width, height, margin, transition} = this.props,
+          { long, delayShort } = transition,
           { chartWidth, chartHeight } = updateSvg(svg, height, width, margin)
 
     appendArea(svg, 'country-chart-area', margin.left, margin.top)
     appendArea(svg, 'country-y-axis', margin.left, margin.top)
     appendArea(svg, 'country-x-axis', margin.left, margin.top + chartHeight)
 
+    this.chartArea = svg.select('.country-chart-area')
+
     this.yAxis = select('.country-y-axis')
     this.yScale = scaleLinear().range([chartHeight, 0]).domain([0, 100])
     this.yAxisCall = axisLeft(this.yScale).tickSizeOuter(0).tickSizeInner(5).tickFormat(format('d')).ticks(chartHeight/100)
+    this.textScale = scaleLinear().range([0, 100]).domain([chartHeight, 0])
 
     this.xAxis = select('.country-x-axis')
-    this.xScale = scaleBand().range([0, chartWidth]).domain(data.map(d => d.country))
+    this.xScale = scaleBand().range([0, chartWidth]).domain(data.map(d => d.country)).padding(.1)
     this.xAxisCall = axisBottom(this.xScale).tickSizeOuter(0).tickSizeInner(5)
 
     createUpdateYAxis( this.yAxis, this.yAxisCall)
     createUpdateXAxis( this.xAxis, this.xAxisCall )
 
+    const dragRects = this.chartArea.selectAll('.dragrect').data(data)
+          //guessText = this.chartArea.selectAll('.guessText').data(countryGuess)
 
+
+    dragRects.enter()
+            .append('rect')
+            .attr('class', `dragrect`)
+            .attr('x', d => this.xScale(d.country))
+            .attr('y', d => this.yScale(0))
+            .attr('dy', -7.5)
+            .attr('width', this.xScale.bandwidth())
+            .attr('height', 0)
+            .call(drag().on("drag", this.handleDrag))
+            .attr('fill', '#333')
+                .merge(dragRects)
+                .transition('dragrects-in')
+                .duration(long)
+                .delay((d,i) => 500 + i * delayShort)
+                .attr('y', d => this.yScale(50))
+                .attr('height', 15)
+
+
+    console.log(Object.values(countryGuess))
+    console.log(Object.keys(countryGuess))
     //console.log(this.yScale.domain(), chartWidth)
 
   }
@@ -62,6 +99,12 @@ Chart.defaultProps = {
   right: 10,
   bottom: 30,
   left: 30
+},
+transition: {
+  long: 1000,
+  start: 2000,
+  delayShort: 200,
+  delayLong: 1000
 }
 }
 
